489eda5ffe6f3caf38c2dc40f0ad802746cf68f1
aidanurp <14maret1998@gmail.com>
2018-07-17 14:39:04 -0700

    mengedit file

---
+++ database/migrations/2018_07_15_164328_create_bukus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBukusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kode');
+            $table->string('judul');
+            $table->string('penerbit');
+            $table->string('penulis');
+            $table->date('tanggal_terbit');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+}
